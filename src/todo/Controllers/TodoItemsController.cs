4a46c6357392d6fedbe9331f7740a807678baf7f
nitark94 <nita.rantaknuuttila@gmail.com>
2025-02-04 12:30:19 +0200

    Päivitetty kohtaan Update the PostTodoItem create method

--- src/todo/Controllers/TodoItemsController.cs
+++ src/todo/Controllers/TodoItemsController.cs
@@ -74,14 +74,15 @@ namespace todo.Controllers
 
         // POST: api/TodoItems
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
-        {
-            _context.TodoItems.Add(todoItem);
-            await _context.SaveChangesAsync();
+       [HttpPost]
+public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+{
+    _context.TodoItems.Add(todoItem);
+    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-        }
+    //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+    return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+}
 
         // DELETE: api/TodoItems/5
         [HttpDelete("{id}")]
