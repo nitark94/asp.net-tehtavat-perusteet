376a1fa75e4501c5ca388565fae0bd7223fb0ced
nitark94 <nita.rantaknuuttila@gmail.com>
2025-02-04 10:43:18 +0200

    Päivitetty kohtaan Register the database context

--- src/todo/Program.cs
+++ src/todo/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
